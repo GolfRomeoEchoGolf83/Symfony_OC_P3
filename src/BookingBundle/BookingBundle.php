68d91ccbb9c67e5d33ca081aeaeb578c7e413eea
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-08 22:12:26 +0100

    erasing default files as well as setting up BookingBundle

--- src/BookingBundle/BookingBundle.php
+++ src/BookingBundle/BookingBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace BookingBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class BookingBundle extends Bundle
 {
 }
