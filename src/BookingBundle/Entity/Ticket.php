4fa0b1543c3ebe36b8c7d3991bdf002d828e746d
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-11-18 10:42:20 +0100

    Class case modification Ticket instead of ticket

--- src/BookingBundle/Entity/Ticket.php
+++ src/BookingBundle/Entity/Ticket.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="ticket")
  * @ORM\Entity(repositoryClass="BookingBundle\Repository\ticketRepository")
  */
-class ticket
+class Ticket
 {
     /**
      * @var int
